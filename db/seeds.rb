efd2b884e24d177d4e276aa58cc80e360bce234c
Aude Ferriere <audeferriere@gmail.com>
2022-11-17 09:01:47 +0100

    push global

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "01 25 56 89 86", category: "belgian"}
+pizza_east = {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "04 25 56 89 58", category: "french"}
+pitaya = {name: "Pitaya", address: "10 rue titi", category: "italian"}
+olafritte = {name: "O'lafritte", address: "10 rue tete", category: "belgian"}
+otacos = {name: "O'tacos", address: "10 rue toto", category: "chinese"}
+
+
+
+
+[dishoom, pizza_east, otacos, olafritte, pitaya].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
